579663b40ea0521063ca158eb2b0142eb500cb36
Sotalia22 <77446230+Sotalia22@users.noreply.github.com>
2024-01-22 11:29:23 +0100

    commit etape 3 - gestion de l'alimentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,22 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-// Définition des pins
-#define SENSOR 33  // Assurez-vous que c'est le pin correct pour le DHT
+#define SENSOR 33  
 #define LED 26
 
-#define DHTPIN SENSOR // Utiliser le pin défini pour le capteur
-#define DHTTYPE DHT11 // ou DHT22 ou DHT21 selon votre capteur
+#define DHTPIN SENSOR 
+#define DHTTYPE DHT11 
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
-uint32_t delayMS;
 
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
   dht.begin();
 
-  // Récupérer les détails du capteur pour obtenir le min_delay
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  delayMS = sensor.min_delay / 1000;
-}
-
-void loop() {
-  digitalWrite(LED, HIGH); // Allume le LED lors de la lecture des données
-
-  // Délai entre les mesures
-  delay(delayMS);
-
-  // Lecture de la température
+  digitalWrite(LED, HIGH); // Allume la LED lors de la lecture des données
+  
+  // Température
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (!isnan(event.temperature)) {
@@ -39,7 +27,7 @@ void loop() {
     Serial.println(F("°C"));
   }
 
-  // Lecture de l'humidité
+  // Humidité
   dht.humidity().getEvent(&event);
   if (!isnan(event.relative_humidity)) {
     Serial.print(F("Humidity: "));
@@ -48,5 +36,12 @@ void loop() {
   }
 
   digitalWrite(LED, LOW); // Éteint le LED après la lecture
-  delay(5000 - delayMS); // Attendre un total de 5 secondes avant la prochaine mesure
+  
+  // Mode Deep Sleep pour 5 secondes
+  esp_sleep_enable_timer_wakeup(5000000);
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  
 }
